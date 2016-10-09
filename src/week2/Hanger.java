2cd644039864ce5b5fbd05cc2655fef67667e665
Jonathan Edwards <edwardjd@colorado.edu>
2016-10-08 18:57:42 -0600

    Array and game loop works

--- src/week2/Hanger.java
+++ src/week2/Hanger.java
@@ -5,7 +5,12 @@ public class Hanger {
 	
 	// constructor
 	public Hanger() {
-		
+		jets = new Jets[5];
+		jets[0] = new Jets("F16", 10000, 200, 2);
+		jets[1] = new Jets("F20", 11000, 300, 3);
+		jets[2] = new Jets("F20", 12000, 400, 4);
+		jets[3] = new Jets("F20", 13000, 500, 5);
+		jets[4] = new Jets("F20", 14000, 600, 6);
 	}
 	
 	//get and set
@@ -15,8 +20,20 @@ public class Hanger {
 	public void setJets(Jets[] j) {
 		this.jets = j;
 	}
-	public void addJet() {
+	
+	public void addJet(String model, double speed, double range, int capacity) {
+		Jets[] addJets = new Jets[jets.length + 1];
+		for (int i = 0; i < addJets.length -1; i++) {
+			addJets[i] = jets[i];
+		}
+		addJets[jets.length] = new Jets(model, speed, range, capacity);
+		jets = addJets;
 		
+		printJets(jets);
+	}
+	public void printJets(Jets[] jet) {
+		for (Jets jets : jet) {
+			System.out.println(jets);
+		}
 	}
 }
-
--- src/week2/JetsTest.java
+++ src/week2/JetsTest.java
@@ -3,29 +3,39 @@ package week2;
 import java.util.*;
 
 public class JetsTest {
+	int choice;
 
 	public static void main(String[] args) {
-		enterJets();
-
+		Scanner kb = new Scanner(System.in);
+		run(kb);
 	}
 
-	public static void enterJets() {
-		Jets[] hangar = new Jets[0];
-		Scanner kb = new Scanner(System.in);
+	public static void run(Scanner kb) {
+		Hanger hanger = new Hanger();
+		int choice = 1;
+		
+		System.out.println("1) Fastest");
+		System.out.println("2) Furthest");
+		System.out.println("3) Add");
+		System.out.println("4) Quit");
+		
+		while (choice != 4) {
+			System.out.print("What do you chose >> ");
+			choice = kb.nextInt();
+			switch (choice) {
+			case 1:
+				enterJets(kb, hanger);
+				break;
+			}
+		}
+	}
+
+	public static void enterJets(Scanner kb, Hanger hanger) {
 		String model;
 		double speed;
 		double range;
 		int capacity;
 
-		// Make Array of Jet Object
-		Jets[] jets = new Jets[5];
-		jets[0] = new Jets("F16", 10000, 200, 2);
-		jets[1] = new Jets("F20", 11000, 300, 3);
-		jets[2] = new Jets("F20", 12000, 400, 4);
-		jets[3] = new Jets("F20", 13000, 500, 5);
-		jets[4] = new Jets("F20", 14000, 600, 6);
-
-		// Create new jets object
 		System.out.println("Enter model:");
 		model = kb.next();
 		System.out.println("Enter speed:");
@@ -34,21 +44,8 @@ public class JetsTest {
 		range = kb.nextDouble();
 		System.out.println("Enter capacity:");
 		capacity = kb.nextInt();
-		kb.close();
 
-		Jets[] addJets = new Jets[jets.length + 1];
-		for (int i = 0; i < addJets.length -1; i++) {
-			addJets[i] = jets[i];
-		}
-		addJets[jets.length] = new Jets(model, speed, range, capacity);
-		jets = addJets;
-		
-		printJets(addJets);
-	}
+		hanger.addJet(model, speed, range, capacity);
 
-	public static void printJets(Jets[] jet) {
-		for (Jets jets : jet) {
-			System.out.println(jets);
-		}
 	}
 }
